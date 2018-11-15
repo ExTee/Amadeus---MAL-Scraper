0a6fe0e9933e7bd4ccd62a2bbb9d89184c678b38
xtwang_cloud <xin.t.wang@mail.mcgill.ca>
2018-11-15 03:27:33 +0000

    modified scrape

--- src/scrape.py
+++ src/scrape.py
@@ -1,10 +1,11 @@
 import utilities
+import sys
 
 def main():
-
+	print (sys.argv)
 	#Loop through all clubs
-	for club_id in range(1,1000):
-		utilities.get_users_from_club(club_id)
+	#for club_id in range(1,1000):
+	#	utilities.get_users_from_club(club_id)
 
 
 
