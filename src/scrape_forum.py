588d07fb4a02734c0bf018be9899ec7445d629ca
Xin Tong Wang <xintong.wang1995@gmail.com>
2018-11-15 16:57:09 -0500

    bugfix

--- src/scrape_forum.py
+++ src/scrape_forum.py
@@ -4,9 +4,11 @@ def main():
 	#Loop through all clubs
 
 	#Serialized run
-	for forum_id in range(3003,1751000):
-		utilities.get_users_from_forum_thread(forum_id)
-#3002 skipped
+	for forum_id in range(3274,1751000):
+		try:
+			utilities.get_users_from_forum_thread(forum_id)
+		except:
+			pass
 
 
 if __name__ == '__main__':
