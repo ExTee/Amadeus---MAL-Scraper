cf4afd4735ff70afcb911f50f272c5f6fac44262
xtwang_cloud <xin.t.wang@mail.mcgill.ca>
2018-11-15 20:59:48 +0000

    added 1-10k

--- src/scrape.py
+++ src/scrape.py
@@ -5,8 +5,8 @@ def main():
 	#Loop through all clubs
 
 	#Serialized run
-	for club_id in range(20000,30000):
-		utilities.get_users_from_club(club_id,"../data/club_usernames/20000-30000/")
+	for club_id in range(30000,100000):
+		utilities.get_users_from_club(club_id,"../data/club_usernames/30000-100000/")
 
 
 '''
